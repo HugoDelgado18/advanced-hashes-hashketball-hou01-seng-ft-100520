d33839e708cf12d475a2237de2476fa47f744bb5
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 23:17:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,7 @@ end
 
 
 
-
-   def num_points_scored(player_name)
+def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
       if player[:player_name] == player_name
